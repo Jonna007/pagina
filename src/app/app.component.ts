c98bf649c8b81ffd3698dc5b67ab9cf6a9e636d8
mateocampo1004 <146900595+mateocampo1004@users.noreply.github.com>
2024-01-23 19:00:22 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from './components/head/head.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FootComponent } from './components/foot/foot.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeadComponent,MainComponent,FooterComponent,FootComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
